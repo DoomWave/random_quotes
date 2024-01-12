8208a3976088202c4c670ec07fa8d5610edb1ce0
DoomWave <123853739+DoomWave@users.noreply.github.com>
2024-01-12 15:32:18 -0800

    mini update

--- js/script.js
+++ js/script.js
@@ -5,24 +5,35 @@ Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
-
+const quotes = 5;
 
 /***
  * `getRandomQuote` function
 ***/
-
+function getRandomQuote(){
+    return
+}
 
 
 /***
  * `printQuote` function
 ***/
-
+function printQuote(){
+    
+}
 
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
+// "Algun dia tocinito"
+//-Gorila Albino alias Skips
+
+//"Around the world, around the world, around the world"
+//-Daft Punk
+
+//"Henshing A go go Baby!"
+//-Joe
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
