6a068f7c27cbffe868c7a8bf5b8976c8ed3679a6
DoomWave <123853739+DoomWave@users.noreply.github.com>
2024-01-15 17:44:56 -0800

    mini update, nothing new, just adding more properties.

--- js/script.js
+++ js/script.js
@@ -6,15 +6,23 @@ Random Quote Generator
  * `quotes` array 
 ***/
 const quotes = [];
-quotes.push("viewtiful","skips","daftpunk","onepiece");
+// quotes.push("viewtiful","skips","daftpunk","onepiece");
+property1: viewtiful, 
+console.log(quotes.length);
+property2: skips,
+console.log(quotes.length);
+property3: daftpunk,
+console.log(quotes.length);
+property4: onepiece,
 console.log(quotes.length);
 
 /***
  * `getRandomQuote` function
 ***/
 function getRandomQuote(){
-    return
+    return Math.floor(Math.random() * max);
 }
+
 
 
 /***
@@ -29,5 +37,6 @@ function printQuote(){
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
+setInterval()
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
