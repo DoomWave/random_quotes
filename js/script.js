1be14bc4bac19a89c69c3f5cb432832e92bf22dc
DoomWave <123853739+DoomWave@users.noreply.github.com>
2024-01-17 18:18:42 -0800

    finished

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ Random Quote Generator
 /* 
  * `quotes` array 
 ***/
-// This line the selection of the quotes 
+// This is an array of quotes. The array contain objects with quotes, source, citation and year.
 const quote = [
     {
         quote:"algun dia tocinito",
@@ -42,16 +42,16 @@ const quote = [
 /***
  * `getRandomQuote` function
 ***/
-// select html by id and sets it to the name quote vox
+// selects html by id and sets it to the name quote-box
 const quoteBox = document.getElementById('quote-box')
-//this selects the quote to put it in the HTML
+//this selects the html by id and sets it to quote button.
 const quoteButton = document.getElementById('load-quote')
-// this function choose in a random way the quotes.
+// this function getRandomQuote gets a quote for the quote array.
 function getRandomQuote(){
-    // this selects shuffle the quotes, to 1-5
+    // randomNumber makes random numbers
     let randomNumber = Math.floor(Math.random() * quote.length)
     // console.log(randomNumber)
-    //this put the quote that was select.
+    //this returns a quote in a random order.
     return quote[randomNumber]
 }
 // console.log(getRandomQuote());
@@ -61,22 +61,23 @@ function getRandomQuote(){
 ***/
 //this function prints the quote.
 function printQuote(){
-// this makes a variant of getRandomQuote to printed 
+// this  a variable called randomQuote that grab other function called getRandomQuote  
     let randomQuote = getRandomQuote()
-    //this line works to print the quote to the HTML
+    //this is a variable called quoteHTML and stores HTML with information from the randomQuote
     let quoteHTML = `
     <p class="quote"> ${randomQuote.quote} </p>
     <p class="source"> ${randomQuote.source}  `
-//This prints the citation to the HTML
+//This is a statement that checks if the randomQuote has the value of citation, if it does, is going to give me the value of the html inside the variable quoteHTML and add the span tag with the class citation.
     if(randomQuote.citation){
         quoteHTML += `<span class="citation">${randomQuote.citation}</span>` 
     }
-    //This prints the year to the HTML
+    //This is a statement that checks if the randomQuote has the value of year, if it does, its going to add span tag with the class year into the HTML.
      if(randomQuote.year){
         quoteHTML += `<span class="year">${randomQuote.citation}</span>`
     }
+    //This closing tag the html
     quoteHTML += `</p>`
-    // This one works to put the quotes in the quotebox
+    // Its selecting the innerHTML and changing the value to the HTML inside the variable quoteHTML
     quoteBox.innerHTML = quoteHTML; 
 }
 // printQuote()
@@ -87,5 +88,5 @@ function printQuote(){
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 // setInterval();
-// this line when the button is clicked, make appears the quote.
+// this line is a event listener and it waits for a click. When there is a click, it will call the printQuote function and the button will work.
 quoteButton.addEventListener("click", printQuote, false);
