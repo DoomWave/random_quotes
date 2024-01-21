0783049f4cf671d5249f9d741ec5c1c2ee7c831e
DoomWave <123853739+DoomWave@users.noreply.github.com>
2024-01-20 18:39:12 -0800

    setinterval function was added and added a eventlistener that changes the color when the button is clicked

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ Random Quote Generator
 /* 
  * `quotes` array 
 ***/
-// This is an array of quotes. The array contain objects with quotes, source, citation and year.
+// This is an array of quotes. The array contain objects with quotes, source, citation, year and tags.
 const quote = [
     {
         quote:"algun dia tocinito",
@@ -47,16 +47,17 @@ const quote = [
 /***
  * `getRandomQuote` function
 ***/
-// selects html by id and sets it to the name quote-box
+//this line selects html by id and sets it to the name quote-box
 const quoteBox = document.getElementById('quote-box')
 //this selects the html by id and sets it to quote button.
 const quoteButton = document.getElementById('load-quote')
+//this selects the body element by id and sets it to the name body
+const body = document.getElementById("body");
+console.log(body)
 
-//document.getElementById("MyElement").className += "Class1,Class2,Class3,Class4,Class5";
-
-// this function getRandomQuote gets a quote for the quote array.
+// this function getRandomQuote gets a quote from the quote array.
 function getRandomQuote(){
-    // randomNumber makes random numbers
+    // randomNumber makes random numbers using the math.random method
     let randomNumber = Math.floor(Math.random() * quote.length)
     // console.log(randomNumber)
     //this returns a quote in a random order.
@@ -68,7 +69,7 @@ function getRandomQuote(){
  * `printQuote` function
 ***/
 //this function prints the quote.
-function printQuote(vj,bacon,crazy,gum,taco){
+function printQuote(){
 // this  a variable called randomQuote that grab other function called getRandomQuote  
     let randomQuote = getRandomQuote()
     //this is a variable called quoteHTML and stores HTML with information from the randomQuote
@@ -90,18 +91,56 @@ function printQuote(vj,bacon,crazy,gum,taco){
         quoteHTML += `<span class="tags">${randomQuote.tags} </span>`
     }
     console.log("tags", randomQuote)
-    //This closing tag the html
+    //This is the closing tag of the html
     quoteHTML += `</p>`
-    // Its selecting the innerHTML and changing the value to the HTML inside the variable quoteHTML
+    // this line selects the innerHTML and changing the value to the HTML inside the variable quoteHTML
     quoteBox.innerHTML = quoteHTML; 
 }
 // printQuote()
 // console.log(printQuote())
+// This function getRandomColor creates random color
+function getRandomColor(){
+    //this line creates a random number from 0 to 256 and then assigns it to value "r"
+    let r = Math.floor(Math.random()* 256 )
+    //this line creates a random number from 0 to 256 and then assigns it to value "b"
+    let g = Math.floor(Math.random()* 256 )
+    //this line creates a random number from 0 to 256 and then assigns it to value "g"
+    let b = Math.floor(Math.random()* 256 )
+    // console.log(randomColorNum)
+    // the color variable combines the value of "r","g" and "b" to create a random color.
+    let color = `rgb(${r},${g},${b})`
+    //this returns the color variable
+    return color
+}
+// this invokes the function gentRandomColor
+getRandomColor() 
+// This makes an event listener and it waits for a click, when its clicked, changes the color of the background
+quoteButton.addEventListener("click", () => {
+    //this line grabs a element called body, changes the style of the background and changes to a random color using the function getRandomColor
+body.style.background = getRandomColor();
+});
+// body.style.backgroundColor = `rgb(${r},${g},${b})`;
+// console.log(body.style.backgroundColor = `rgb(${r},${g},${b})`);
 
+console.log(getRandomColor())
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
-// setInterval();
+// function quoteRefresh(){
+//     const intervalID = 
+//     intervalID = setInterval(getRandomColor, 10);
+// }
+//
+const interval = () => {
+    return setInterval(() =>{
+        printQuote() 
+        body.style.background = getRandomColor(); 
+    }, 5000)
+}
+interval()
+
+
+
 // this line is a event listener and it waits for a click. When there is a click, it will call the printQuote function and the button will work.
 quoteButton.addEventListener("click", printQuote, false);
